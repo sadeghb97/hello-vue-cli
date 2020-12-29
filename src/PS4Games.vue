1f2113d11c7e81c3832699719b147c1d146dd459
Sadegh Bagherzadeh <sadeghb97@gmail.com>
2020-12-29 08:22:33 +0330

    add title to routes and use vuerouter

--- src/PS4Games.vue
+++ src/PS4Games.vue
@@ -2,7 +2,7 @@
     <html>
         <head>
             <meta charset="utf8">
-            <title>Vue Slot Template</title>
+            <title>Vue PS4 Games</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         </head>
         <body>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,43 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import PS4Games from './PS4Games.vue'
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
+Vue.use(VueRouter)
 
-const routes = {
-  '/': App,
-  '/about': HelloWorld,
-  '/ps4games': PS4Games
-}
+const routes = [
+  {path: '/', component: App, name: "home", meta:{title: "Home"}},
+  {path: '/about', component: HelloWorld, name: "about", meta:{title: "About"}},
+  {path: '/ps4games', component: PS4Games, name: "ps4", meta:{title: "PS4Games"}}
+]
+const router = new VueRouter({
+  routes: routes
+})
 
 new Vue({
-  el: '#app',
+  router: router,
   data: {
-    currentRoute: window.location.pathname
+    curRoute: window.location.pathname
   },
-  computed: {
-    ViewComponent () {
-      return routes[this.currentRoute] || App
+  computed:{
+    CurrentRoute(){
+      var curRoute = this.curRoute;
+      return routes.find(function(x){
+        return x.path == curRoute
+      }) || routes[0]
+    },
+    ViewComponent(){
+      return this.CurrentRoute.component
+    },
+    Title(){
+      return this.CurrentRoute.meta.title
     }
   },
+  created: function(){
+    document.title = this.Title
+  },
   render (h) { return h(this.ViewComponent) }
-})
+}).$mount('#app')
+
